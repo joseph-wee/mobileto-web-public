86ac33096641ddb4dc975b1f5106b14e30ab63e2
Wee Yosup <weeyosup@Weeui-iMac.local>
2024-09-15 21:07:04 +0900

    build: 빌드 설정 세팅

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  /** 정적 페이지로 배포시 활성화 */
+  // output: 'export',
+  // trailingSlash: true,
+  // images: {
+  //   unoptimized: true
+  // }
+
 };
 
 export default nextConfig;
