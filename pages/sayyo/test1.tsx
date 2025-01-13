9c09539714dbd9e185dc50196fd129493514b5b8
Wee Yosup <weeyosup@Weeui-iMac.local>
2025-01-13 15:00:50 +0900

    feat: web share post 페이지 임시 구현 applicants 포함

---
+++ pages/sayyo/test1.tsx
@@ -0,0 +1,484 @@
+import React, { useEffect, useState } from "react";
+
+import Image from "next/image";
+
+import {
+  ic_document,
+  ic_pay,
+  ic_pin,
+  ic_share_dark,
+  img_sample,
+  logo_sayyo_white,
+  sample_applicant1,
+  sample_applicant2,
+  sample_applicant3,
+  sample_applicant4,
+} from "@/assets";
+
+const useTest1 = () => {
+  const [share, setShare] = useState(false); // 공유하기 ticker 값
+
+  // TODO: 나중에 앱링크, 유니버셜링크 받아서 브라우저별 세팅 해야 함.
+  const [path, setPath] = useState(
+    "https://play.google.com/store/apps/details?id=com.teambro.sayyo"
+  ); // 링크 주소
+
+  /** url 복사 및 팝업 */
+  const shareHandler = () => {
+    if (share) {
+      return;
+    }
+    navigator.clipboard.writeText(path);
+    setShare(true);
+    setTimeout(() => setShare(false), 2000);
+  };
+
+  /** 사용자 ios일 경우 세팅 */
+  const iosSetter = () => {
+    const osInfo = navigator.userAgent;
+    const iosDevice = ["iPhone", "iPad", "iPod", "Mac"];
+
+    for (const el of iosDevice) {
+      if (osInfo.includes(el)) {
+        // app store 에 올라오면 링크 세팅
+        // setPath("ios");
+        return;
+      }
+    }
+  };
+
+  if (typeof window !== "undefined") {
+    window.onresize = () => console.log("test");
+  }
+
+  /** applicants list */
+  const applicants = [
+    { image: sample_applicant1, name: "Fruity", loc: "Quan1" },
+    { image: sample_applicant2, name: "Grace", loc: "Quan1" },
+    { image: sample_applicant3, name: "works", loc: "Quan5" },
+    { image: sample_applicant4, name: "LOKAA", loc: "The duc" },
+    { image: sample_applicant1, name: "Fruity", loc: "Quan1" },
+    { image: sample_applicant2, name: "Grace", loc: "Quan1" },
+    { image: sample_applicant3, name: "works", loc: "Quan5" },
+    { image: sample_applicant4, name: "LOKAA", loc: "The duc" },
+    { image: sample_applicant1, name: "Fruity", loc: "Quan1" },
+    { image: sample_applicant2, name: "Grace", loc: "Quan1" },
+    { image: sample_applicant3, name: "works", loc: "Quan5" },
+    { image: sample_applicant4, name: "LOKAA", loc: "The duc" },
+  ];
+
+  /** 렌더링시 실행 */
+  useEffect(() => {
+    iosSetter();
+  }, []);
+
+  return (
+    <div className=" bg-[#F2F6F8] font-Roboto">
+      <div
+        className="flex flex-col min-h-[100vh] max-w-[768px] mx-auto border-x-[1px] border-x-[#DEE6EC] box-content
+      md768:border-none
+      "
+      >
+        {/** ticker */}
+        <div
+          className={
+            share
+              ? `opacity-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center rounded-[8px] bg-[#000000]/70
+        h-[59px] w-[239px]
+        text-[16px] leading-[20.8px] text-sayyo_wht
+        transition-all
+          `
+              : `opacity-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex justify-center items-center rounded-[8px] bg-[#000000]/70
+        h-[59px] w-[239px]
+        text-[16px] leading-[20.8px] text-sayyo_wht transition-all`
+          }
+        >
+          Share url copied successfully
+        </div>
+        {/** 헤더 */}
+        <header className="h-[56px] pl-[16px] pr-[19px] flex justify-between items-center bg-sayyo_wht">
+          <Image
+            src={logo_sayyo_white}
+            alt="logo_sayyo_white"
+            className="w-[58px] h-[26px]"
+          />
+          <Image
+            src={ic_share_dark}
+            alt="ic_share_dark"
+            onClick={() => shareHandler()}
+            className="w-[24px] h-[24px] cursor-pointer"
+          />
+        </header>
+        {/** 내용 */}
+        <div className="flex-1">
+          <div className="mb-[20px] p-[16px] bg-sayyo_wht">
+            <h1 className="mb-[8px] text-sayyo_l2 text-[22px] font-bold leading-[28.6px]">
+              vietnamese language teacher need
+            </h1>
+            <p className="mb-[12px] text-sayyo_l2 leading-[18.2px]">
+              vietnamese language teacher need vietnamese language teacher
+              needvietnamese language teacher need vietnamese language teacher
+              need
+            </p>
+            <div className="mb-[12px] border-t-[1px] border-sayyo_bg_more border-dashed" />
+            <div className="mb-[12px]">
+              <div className="mb-[6px] flex items-center gap-[8px]">
+                <Image src={ic_pay} alt="ic_pay" />
+                <div className="text-sayyo_l2 text-[14px] leading-[16.8px]">
+                  3.000.000đ
+                </div>
+              </div>
+              <div className="mb-[6px] flex items-center gap-[8px]">
+                <Image src={ic_document} alt="ic_document" />
+                <div className="flex items-center gap-[6px] text-sayyo_l2 text-[14px] leading-[16.8px]">
+                  <span>Man</span>
+                  <div className="h-[10px] border-l-[1px] border-[#000000]/10" />
+                  <span>12 year exp</span>
+                  <div className="h-[10px] border-l-[1px] border-[#000000]/10" />
+                  <span>2 people</span>
+                </div>
+              </div>
+              <div className="flex items-center gap-[8px]">
+                <Image src={ic_pin} alt="ic_pin" />
+                <div className="text-sayyo_l2 text-[14px] leading-[16.8px]">
+                  Bến Nghé, Quận1
+                </div>
+              </div>
+            </div>
+
+            <Image
+              src={img_sample}
+              alt="sample"
+              className="max-h-[217px] rounded-[9px] cursor-pointer"
+              onClick={() => window.open(path)}
+            />
+          </div>
+          {/** applicnats */}
+          <div
+            className="mb-[20px] pt-[9.5px] h-[167px] box-border bg-sayyo_wht cursor-pointer"
+            onClick={() => window.open(path)}
+          >
+            <div className="flex justify-between items-center mb-[19px]">
+              <div className="flex pl-[20px] text-[16px] font-bold leading-[20.8px]">
+                Aplicants&nbsp;<span className="font-normal">9</span>
+              </div>
+              <div className="pr-[21px] text-sayyo_subtext text-[12px] leading-[15.6px]">
+                {" "}
+                More
+              </div>
+            </div>
+            {/** overflow hidden */}
+            <div
+              className="grid gap-[12px] justify-between px-[16px] h-[95.42px] overflow-hidden
+            grid-cols-11
+            md767:grid-cols-10
+            md699:grid-cols-9
+            md631:grid-cols-8
+            md563:grid-cols-7
+            md495:grid-cols-6
+            md427:grid-cols-5
+            "
+            >
+              {/** 전체 너비 */}
+              {applicants.map((i, j) => {
+                return (
+                  !(applicants.length > 11 && j > 9) && (
+                    <div key={`${j}546`} className="md767:hidden">
+                      <div>
+                        <Image
+                          src={i.image}
+                          alt="sample"
+                          className="rounded-full mb-[8.42px] w-[56px] h-[56px]
+                        
+                        "
+                        />
+                        <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                          {`${i.name}`}
+                        </div>
+                        <div className="flex items-center justify-center text-sayyo_subtext text-[12px] leading-[15.6px]">
+                          {`${i.loc}`}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+              {applicants.length > 11 && (
+                <div className="md767:hidden">
+                  <div
+                    className={`flex items-center justify-center rounded-full mb-[8.42px] w-[56px] h-[56px] bg-sayyo_subtext text-sayyo_wht text-[16px] leading-[16px]
+         
+                      `}
+                  >
+                    +{applicants.length - 10}
+                  </div>
+                  <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                    More
+                  </div>
+                </div>
+              )}
+              {/** 767 이하 */}
+              {applicants.map((i, j) => {
+                return (
+                  !(applicants.length > 10 && j > 8) && (
+                    <div
+                      key={`${j}546`}
+                      className="hidden md767:block md699:hidden"
+                    >
+                      <div>
+                        <Image
+                          src={i.image}
+                          alt="sample"
+                          className="rounded-full mb-[8.42px] w-[56px] h-[56px]
+                        
+                        "
+                        />
+                        <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                          {`${i.name}`}
+                        </div>
+                        <div className="flex items-center justify-center text-sayyo_subtext text-[12px] leading-[15.6px]">
+                          {`${i.loc}`}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+              {applicants.length > 10 && (
+                <div className="hidden md767:block md699:hidden">
+                  <div
+                    className={`flex items-center justify-center rounded-full mb-[8.42px] w-[56px] h-[56px] bg-sayyo_subtext text-sayyo_wht text-[16px] leading-[16px]
+         
+                      `}
+                  >
+                    +{applicants.length - 9}
+                  </div>
+                  <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                    More
+                  </div>
+                </div>
+              )}
+              {/** 699 이하 */}
+              {applicants.map((i, j) => {
+                return (
+                  !(applicants.length > 9 && j > 7) && (
+                    <div
+                      key={`${j}546`}
+                      className="hidden md699:block md631:hidden"
+                    >
+                      <div>
+                        <Image
+                          src={i.image}
+                          alt="sample"
+                          className="rounded-full mb-[8.42px] w-[56px] h-[56px]
+                        
+                        "
+                        />
+                        <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                          {`${i.name}`}
+                        </div>
+                        <div className="flex items-center justify-center text-sayyo_subtext text-[12px] leading-[15.6px]">
+                          {`${i.loc}`}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+              {applicants.length > 9 && (
+                <div className="hidden md699:block md631:hidden">
+                  <div
+                    className={`flex items-center justify-center rounded-full mb-[8.42px] w-[56px] h-[56px] bg-sayyo_subtext text-sayyo_wht text-[16px] leading-[16px]
+         
+                      `}
+                  >
+                    +{applicants.length - 8}
+                  </div>
+                  <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                    More
+                  </div>
+                </div>
+              )}
+              {/** 631 이하 */}
+              {applicants.map((i, j) => {
+                return (
+                  !(applicants.length > 8 && j > 6) && (
+                    <div
+                      key={`${j}546`}
+                      className="hidden md631:block md563:hidden"
+                    >
+                      <div>
+                        <Image
+                          src={i.image}
+                          alt="sample"
+                          className="rounded-full mb-[8.42px] w-[56px] h-[56px]
+                        
+                        "
+                        />
+                        <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                          {`${i.name}`}
+                        </div>
+                        <div className="flex items-center justify-center text-sayyo_subtext text-[12px] leading-[15.6px]">
+                          {`${i.loc}`}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+              {applicants.length > 8 && (
+                <div className="hidden md631:block md563:hidden">
+                  <div
+                    className={`flex items-center justify-center rounded-full mb-[8.42px] w-[56px] h-[56px] bg-sayyo_subtext text-sayyo_wht text-[16px] leading-[16px]
+         
+                      `}
+                  >
+                    +{applicants.length - 7}
+                  </div>
+                  <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                    More
+                  </div>
+                </div>
+              )}
+              {/** 563 이하 */}
+              {applicants.map((i, j) => {
+                return (
+                  !(applicants.length > 7 && j > 5) && (
+                    <div
+                      key={`${j}546`}
+                      className="hidden md563:block md495:hidden"
+                    >
+                      <div>
+                        <Image
+                          src={i.image}
+                          alt="sample"
+                          className="rounded-full mb-[8.42px] w-[56px] h-[56px]
+                        
+                        "
+                        />
+                        <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                          {`${i.name}`}
+                        </div>
+                        <div className="flex items-center justify-center text-sayyo_subtext text-[12px] leading-[15.6px]">
+                          {`${i.loc}`}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+              {applicants.length > 7 && (
+                <div className="hidden md563:block md495:hidden">
+                  <div
+                    className={`flex items-center justify-center rounded-full mb-[8.42px] w-[56px] h-[56px] bg-sayyo_subtext text-sayyo_wht text-[16px] leading-[16px]
+         
+                      `}
+                  >
+                    +{applicants.length - 6}
+                  </div>
+                  <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                    More
+                  </div>
+                </div>
+              )}
+              {/** 495 이하 */}
+              {applicants.map((i, j) => {
+                return (
+                  !(applicants.length > 6 && j > 4) && (
+                    <div
+                      key={`${j}546`}
+                      className="hidden md495:block md427:hidden"
+                    >
+                      <div>
+                        <Image
+                          src={i.image}
+                          alt="sample"
+                          className="rounded-full mb-[8.42px] w-[56px] h-[56px]
+                        
+                        "
+                        />
+                        <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                          {`${i.name}`}
+                        </div>
+                        <div className="flex items-center justify-center text-sayyo_subtext text-[12px] leading-[15.6px]">
+                          {`${i.loc}`}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+              {applicants.length > 6 && (
+                <div className="hidden md495:block md427:hidden">
+                  <div
+                    className={`flex items-center justify-center rounded-full mb-[8.42px] w-[56px] h-[56px] bg-sayyo_subtext text-sayyo_wht text-[16px] leading-[16px]
+         
+                      `}
+                  >
+                    +{applicants.length - 5}
+                  </div>
+                  <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                    More
+                  </div>
+                </div>
+              )}
+              {/** 427 이하 */}
+              {applicants.map((i, j) => {
+                return (
+                  !(applicants.length > 5 && j > 3) && (
+                    <div key={`${j}546`} className="hidden md427:block">
+                      <div>
+                        <Image
+                          src={i.image}
+                          alt="sample"
+                          className="rounded-full mb-[8.42px] w-[56px] h-[56px]
+                        
+                        "
+                        />
+                        <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                          {`${i.name}`}
+                        </div>
+                        <div className="flex items-center justify-center text-sayyo_subtext text-[12px] leading-[15.6px]">
+                          {`${i.loc}`}
+                        </div>
+                      </div>
+                    </div>
+                  )
+                );
+              })}
+              {applicants.length > 5 && (
+                <div className="hidden md427:block">
+                  <div
+                    className={`flex items-center justify-center rounded-full mb-[8.42px] w-[56px] h-[56px] bg-sayyo_subtext text-sayyo_wht text-[16px] leading-[16px]
+         
+                      `}
+                  >
+                    +{applicants.length - 4}
+                  </div>
+                  <div className="flex items-center justify-center text-sayyo_l2 text-[12px] leading-[15.6px]">
+                    More
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+          {/** 하단 버튼 */}
+        </div>
+        <div
+          className="p-[16px] bottom-0 max-w-[768px] w-full bg-sayyo_wht border-t-[1px] border-t-[#DEE6EC]
+          
+          "
+        >
+          <button
+            className="w-full h-[48px] text-sayyo_wht text-[14px] font-bold leading-[18.2px] rounded-[8px] bg-sayyo_primary"
+            onClick={() => window.open(path)}
+          >
+            Apply
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default useTest1;
