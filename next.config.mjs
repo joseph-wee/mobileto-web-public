0dbcabf19912afe2baf4299993fc472bcf3685e5
Wee Yosup <weeyosup@Weeui-iMac.local>
2025-01-24 23:09:21 +0900

    set: 이미지 호스트 네임 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,24 @@
 /** @type {import('next').NextConfig} */
 
+"https://static.sayyo.mobileto.io/images/20250121131414-a5f7db12b8f445d9a67b813bebfb81b0.png"
+
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.sayyo.mobileto.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dev-static.sayyo.mobileto.io',
+        port: '',
+        pathname: '/**',
+      },
+    ]
+  },
   reactStrictMode: true,
   /** 정적 페이지로 배포시 활성화 */
   // output: 'export',
