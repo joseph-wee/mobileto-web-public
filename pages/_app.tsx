e3e11e498a7df9f60870781d16065827a6356470
Joseph <starlightkor@gmail.com>
2025-05-12 17:20:36 +0900

    feat: GoogleAnalytics 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import GoogleAnalytics from "@/lib/GoogleAnalytics";
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
@@ -5,6 +6,9 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      {process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS && (
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS} />
+      )}
       <Component {...pageProps} />
     </>
   );
