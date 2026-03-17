aa14f2636756bad29123b7e8e490d932b6cb8084
Joseph <starlightkor@gmail.com>
2026-03-17 14:03:25 +0900

    Revert "chore: 필요없는 이미지들 삭제" + 나머지 작업 되돌리기
    This reverts commit 709a2f59a98b72903ecc44a01cff122fc205ea39.

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,31 @@
 /** @type {import('next').NextConfig} */
 
+"https://static.sayyo.mobileto.io/images/20250121131414-a5f7db12b8f445d9a67b813bebfb81b0.png"
+
 const nextConfig = {
-  // reactStrictMode: true,
-  /** 정적 페이지로 배포시 활성화 */
-  output: 'export',
-  trailingSlash: true,
   images: {
-    unoptimized: true
-  }
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.sayyo.mobileto.io',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dev-static.sayyo.mobileto.io',
+        port: '',
+        pathname: '/**',
+      },
+    ]
+  },
+  reactStrictMode: true,
+  /** 정적 페이지로 배포시 활성화 */
+  // output: 'export',
+  // trailingSlash: true,
+  // images: {
+  //   unoptimized: true
+  // }
 
 };
 
