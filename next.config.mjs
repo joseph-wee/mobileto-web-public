ed354a3be413d9348c7200f38389ea177a8c15ab
Joseph <starlightkor@gmail.com>
2026-03-17 14:21:52 +0900

    deploy: url 주소 설정 변경

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
   // reactStrictMode: true,
   /** 정적 페이지로 배포시 활성화 */
   output: 'export',
-  trailingSlash: true,
+  trailingSlash: false,
   images: {
     unoptimized: true
   }
